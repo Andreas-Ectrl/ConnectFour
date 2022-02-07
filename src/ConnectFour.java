a19f69e467dcb049bf2d759f53f4837c52c3f533
AndreasE <75861921+FingoA@users.noreply.github.com>
2022-02-07 18:32:43 +0100

    Format: add Space game.init()

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -1,7 +1,7 @@
 public class ConnectFour {
 
     public static void main(String[] args) {
-        Game.init(6,7, 50);
+        Game.init(6, 7, 50);
         Game.loop();
         System.exit(0);
     }
